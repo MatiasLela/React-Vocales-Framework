dbd1a106e6e7a6607c4d365eb9dbebe990eee520
MatiasLela <matiaslela0@gmail.com>
2023-03-28 01:54:05 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className='d-flex justify-content-center'>Contador de vocales React</h2>
+      <div className='card mr-auto p-4'>
+        <div className='card-body'>
+          <form onSubmit={vocales} className='input-group '>
+            <input type="text" name="texto" className='form-control' placeholder='Introduzca una frase o una palabra'></input>
+            <div className='input-group-append'>
+              <button type="submit" className='btn btn-outline-success'>Subir</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <footer className='d-flex justify-content-center text-muted'>Hecho por Matias Lela</footer>
     </div>
   );
+}
+
+function vocales(e){
+  e.preventDefault();
+  const texto = e.target.texto.value;
+  const vocales = 'aeiouAEIOU';
+  let contador = 0;
+  for(let i = 0; i<texto.length; i++){
+    if(vocales.includes(texto[i])) contador++;
+  }
+
+  alert("Cantidad de vocales: "+contador);
 }
 
 export default App;
